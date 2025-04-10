0475d4163ce7714a06e960a71f25a486fce1aead
Priyanka <ppundir370@gmail.com>
2025-04-10 23:31:04 +0530

    commit -m "Assignment 1 and 2"

---
+++ Assignment1.py
@@ -0,0 +1,17 @@
+print(">>> Starting script")
+
+num1 = float(input("Enter the num1: "))
+num2 = float(input("Enter the num2: "))
+
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+
+if num2 != 0:
+    print("Division:", num1 / num2)
+else:
+    print("Division: Cannot divide by 0")
+
+
+
---
+++ Assignment2.py
@@ -0,0 +1,9 @@
+# Taking user's first and last name as input
+f_name = input("Enter your first name: ")
+l_name = input("Enter your last name: ")
+
+# Concatenating full name
+full_name = f_name + " " + l_name
+
+# Printing personalized greeting
+print("Hello, " + full_name + "! Welcome to the Python world.")
