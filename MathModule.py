ab2cdb3a7aa32fd0d478133a1a70c1a5ab59ab8a
Priyanka <ppundir370@gmail.com>
2025-04-14 23:17:09 +0530

    Adding Factorial and MathModule

---
+++ MathModule.py
@@ -0,0 +1,11 @@
+import math
+num = int(input("Enter the number : "))
+
+sq = math.sqrt(num)
+log = math.log(num)
+sine = math.sin(num)
+
+print("Sqaure root is : " , sq)
+print("Logarithm : ", log)
+print("Sine : " , sine)
+
